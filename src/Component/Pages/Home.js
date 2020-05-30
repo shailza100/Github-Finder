8ef3861f63f832a93291161d27ca01b293c80aa5
burmanshailza@gmail.com <burmanshailza@gmail.com>
2020-05-30 16:08:51 +0530

    Netlify files added

---
+++ src/Component/Pages/Home.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import Search from "../User/Search.js";
+import Users from "../User/Users.js";
+
+const Home = () => (
+  <Fragment>
+    <Search />
+    <Users />
+  </Fragment>
+);
+
+export default Home;
---
+++ src/Component/Pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>The page you are looking for does not exist.</h1>
+    </div>
+  );
+};
+export default NotFound;
